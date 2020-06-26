4b0b6c0ec7b1f48ba01e6b5d679db373cb12010b
Yogeshwar Venkatesh <yogeshwar@thoughtclan.com>
2020-06-26 18:13:25 +0530

    Add serilog

--- Program.cs
+++ Program.cs
@@ -1,21 +1,50 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 using video_streaming_service.Configuration;
+using video_streaming_service.Configuration.Serilog;
 
 namespace video_streaming_service
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            const string logFormat = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message} (at {Caller}){NewLine}{Exception}";
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(outputTemplate: logFormat)
+                .WriteTo.File("logs\\log.txt", rollingInterval: RollingInterval.Day, outputTemplate: logFormat)
+                .CreateLogger();
+
             EnvironmentConfiguration.Build();
 
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Information("Starting video streaming service");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Application terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     config.AddEnvironmentVariables(prefix: "VAP_");
