f6349cb3d0ff89197875011da95d74069cbcfcd8
Yogeshwar Venkatesh <yogeshwar@thoughtclan.com>
2020-06-29 09:44:08 +0530

    Fix switching streams and handle video not yet being ready for playback

--- ClientApp/src/components/VideoPlayer/VideoPlayer.tsx
+++ ClientApp/src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,73 +1,85 @@
 import React from 'react';
-import videojs, {VideoJsPlayer, VideoJsPlayerOptions} from "video.js";
+import videojs, {VideoJsPlayer} from "video.js";
+import StreamInfo from "../../models/StreamInfo";
 
 import 'video.js/dist/video-js.min.css';
 
 import './VideoPlayer.scss';
-import StreamInfo from "../../models/StreamInfo";
 
 export default function VideoPlayer() {
     const videoNode = React.useRef<null | HTMLVideoElement>(null);
     const player = React.useRef<null | VideoJsPlayer>(null);
     const streamsRefreshInterval = React.useRef<null | NodeJS.Timeout>(null);
-    
+    const streamLoadRetryTimeout = React.useRef<null | NodeJS.Timeout>(null);
+
     const [selectedStream, setSelectedStream] = React.useState<null | StreamInfo>(null);
     const [streamsList, setStreamsList] = React.useState<StreamInfo[]>([]);
-    
+
     const loadStreamsList = async () => {
         try {
             const response = await fetch('/streams');
             const json = await response.json();
-            
+
             setStreamsList(json);
         } catch (e) {
             alert(e.message);
             console.error(e);
         }
     };
-    
+
+    const loadStream = async (stream: StreamInfo) => {
+        if (player.current && player.current.src().length > 0) {
+            await player.current.pause();
+            player.current.src([]);
+        }
+
+        try {
+            const response = await fetch(stream.publicUri);
+
+            if (!player.current)
+                player.current = videojs(videoNode.current, {
+                    controls: true,
+                    sources: [],
+                });
+
+            if (response.status === 200) {
+                // Stream is present and can be added as a source
+                player.current.src(stream.publicUri);
+                await player.current.play();
+            } else {
+                // Stream failed to load for whatever reason, set a delay to retry once more
+                streamLoadRetryTimeout.current = setTimeout(() => loadStream(stream), 2500);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     const selectStream = (stream: StreamInfo) => {
         setSelectedStream(stream);
     };
-    
+
     React.useEffect(() => {
-        if (!selectedStream && player.current) {
-            player.current.dispose();
-            player.current = null;
-            
-            return;
-        }
-        
-        if (selectedStream === null)
+        if (!selectedStream)
             return;
 
-        const videoOptions: VideoJsPlayerOptions = {
-            controls: true,
-            muted: true,
-            autoplay: true,
-            sources: [{
-                src: selectedStream.publicUri,
-                type: 'application/vnd.apple.mpegurl',
-            }],
-        };
-
-        player.current = videojs(videoNode.current, videoOptions);
+        loadStream(selectedStream);
     }, [selectedStream]);
-    
+
     React.useEffect(() => {
         loadStreamsList();
-        
+
         setInterval(loadStreamsList, 3000);
-        
+
         return () => {
             if (player.current)
                 player.current.dispose();
-            
+
             if (streamsRefreshInterval.current)
                 clearInterval(streamsRefreshInterval.current);
         };
     }, []);
-    
+
     return (
         <div className='player-wrapper'>
             <ul className='streams-list'>
@@ -79,13 +91,13 @@ export default function VideoPlayer() {
                             </button>
                         </li>
                     ))
-                } 
+                }
             </ul>
-            
+
             <div className='current-stream-info'>
                 <h3>{selectedStream ? selectedStream.name : null}</h3>
             </div>
-            
+
             <div data-vjs-player>
                 <video
                     ref={videoNode}
